22c0f34f56cf0309e6ed9f0df6a31739e03634d3
Viniciuss12 <112558199+Viniciuss12@users.noreply.github.com>
2025-04-22 13:19:15 -0300

    Update msalConfig.js

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "375e806e-8876-4de3-ac21-d6c2d725ca35",
     authority: "https://login.microsoftonline.com/ab418361-f6c6-4534-b75d-e2d7fcfe5d44",
-    redirectUri: "https://fila-impressao-3d.onrender.com"
+    redirectUri: "http://localhost:3000"
   },
   cache: {
     cacheLocation: "sessionStorage",
