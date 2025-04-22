1a1ad759a8ad5d4deaef1f3e20a5f9976d0e6c0e
Viniciuss12 <112558199+Viniciuss12@users.noreply.github.com>
2025-04-22 14:28:16 -0300

    Update msalConfig.js

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -1,8 +1,8 @@
 const msalConfig = {
   auth: {
-    clientId: "375e806e-8876-4de3-ac21-d6c2d725ca35", // seu clientId
-    authority: "https://login.microsoftonline.com/ab418361-f6c6-4534-b75d-e2d7fcfe5d44", // seu tenantId
-    redirectUri: "https://fila-impressao-3d.onrender.com", // deve ser exatamente igual ao da Azure
+    clientId: "375e806e-8876-4de3-ac21-d6c2d725ca35",
+    authority: "https://login.microsoftonline.com/ab418361-f6c6-4534-b75d-e2d7fcfe5d44",
+    redirectUri: "https://fila-impressao-3d.onrender.com",
   },
   cache: {
     cacheLocation: "localStorage",
